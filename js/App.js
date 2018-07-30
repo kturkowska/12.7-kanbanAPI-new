c8784e01dccbf8fc7ff0c784d49e2d58a4ef1c08
Katarzyna Turkowska <ka.turkowska@gmail.com>
2018-07-30 22:18:11 +0200

    mać

---
+++ js/App.js
@@ -0,0 +1,35 @@
+var baseUrl = 'https://kodilla.com/pl/bootcamp-api';
+var myHeaders = {
+	'X-Client-Id': '3433',
+	'X-Auth-Token': '89e979655a71175638c2eb6125900036'
+};
+fetch(baseUrl + '/board', {
+	headers: myHeaders
+}).then(function (resp) {
+	return resp.json();
+}).then(function (resp) {
+	setupColumns(resp.columns);
+});
+
+function setupColumns(columns) {
+	columns.forEach(function (column) {
+		var col = new Column(column.id, column.name);
+		board.addColumn(col);
+		setupCards(col, column.cards);
+	});
+}
+
+function setupCards(col, cards) {
+	cards.forEach(function (card) {
+		var cardObj = new Card(card.id, card.name);
+		col.addCard(cardObj);
+	});
+}
+
+function generateTemplate(name, data, basicElement) {
+	var template = document.getElementById(name).innerHTML;
+	var element = document.createElement(basicElement || 'div');
+	Mustache.parse(template);
+	element.innerHTML = Mustache.render(template, data);
+	return element;
+}
